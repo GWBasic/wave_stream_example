358dbf5e69e3efa7de64983cfd78e34e8b200811
Andrew Rondeau <fhjraklfhdsajklfadsjkl@fdsjkfndsajkfndasjkln.fds>
2023-04-30 10:41:23 -0400

    Refactored to handle changes in 28-support-51-and-other-channel-layouts

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ use std::io::Result;
 use std::path::Path;
 
 use wave_stream::open_wav::OpenWav;
-use wave_stream::wave_header::{SampleFormat, WavHeader};
+use wave_stream::samples_by_channel::SamplesByChannel;
+use wave_stream::wave_header::{SampleFormat, WavHeader, Channels};
 use wave_stream::wave_reader::{RandomAccessOpenWavReader, StreamOpenWavReader};
 use wave_stream::{read_wav_from_file_path, write_wav_to_file_path};
 
@@ -13,7 +14,7 @@ fn main() {
     // Inspect metadata
     // ******************************
     println!("Number of channels: {0}, samples per second: {1}, bits per sample: {2}, length in samples: {3}",
-        open_wav.channels(),
+        open_wav.num_channels(),
         open_wav.bits_per_sample(),
         open_wav.sample_rate(),
         open_wav.len_samples());
@@ -21,8 +22,11 @@ fn main() {
     // Read via random access
     // ******************************
     let mut random_access_wave_reader = open_wav.get_random_access_f32_reader().unwrap();
-    let first_sample = random_access_wave_reader.read_sample(0, 0).unwrap();
-    println!("First sample, channel 0: {0}", first_sample);
+    let first_sample = random_access_wave_reader.read_sample(0).unwrap();
+    println!(
+        "First sample, front_left: {0}",
+        first_sample.front_left.expect("front_left missing")
+    );
 
     // Read via an enumerable: Find the loudest sample in the wave file
     // ******************************
@@ -42,7 +46,7 @@ fn main() {
     for samples_result in iterator {
         let samples = samples_result.unwrap();
 
-        for sample in samples {
+        for sample in samples.to_vec() {
             loudest_sample = f32::max(loudest_sample, sample);
         }
     }
@@ -51,10 +55,28 @@ fn main() {
 
     // Write via random access
     // ******************************
-    let sample_rate= 96000;
+    let sample_rate = 96000;
     let header = WavHeader {
         sample_format: SampleFormat::Float,
-        channels: 1,
+        channels: Channels {
+            front_left: true,
+            front_right: false,
+            front_center: false,
+            low_frequency: false,
+            back_left: false,
+            back_right: false,
+            front_left_of_center: false,
+            front_right_of_center: false,
+            back_center: false,
+            side_left: false,
+            side_right: false,
+            top_center: false,
+            top_front_left: false,
+            top_front_center: false,
+            top_front_right: false,
+            top_back_left: false,
+            top_back_center: false,
+            top_back_right: false },
         sample_rate,
     };
 
@@ -75,7 +97,25 @@ fn main() {
         let modulo = (sample % samples_in_ramp) as f32;
         let sample_value = (2f32 * modulo / samples_in_ramp_f32) - 1f32;
         random_access_wave_writer
-            .write_sample(sample, 0, sample_value)
+            .write_samples(sample, SamplesByChannel {
+                front_left: Some(sample_value),
+                front_right: None,
+                front_center: None,
+                low_frequency: None,
+                back_left: None,
+                back_right: None,
+                front_left_of_center: None,
+                front_right_of_center: None,
+                back_center: None,
+                side_left: None,
+                side_right: None,
+                top_center: None,
+                top_front_left: None,
+                top_front_center: None,
+                top_front_right: None,
+                top_back_left: None,
+                top_back_center: None,
+                top_back_right: None })
             .unwrap();
     }
 
@@ -85,7 +125,25 @@ fn main() {
     // ******************************
     let header = WavHeader {
         sample_format: SampleFormat::Float,
-        channels: 1,
+        channels: Channels {
+            front_left: true,
+            front_right: false,
+            front_center: false,
+            low_frequency: false,
+            back_left: false,
+            back_right: false,
+            front_left_of_center: false,
+            front_right_of_center: false,
+            back_center: false,
+            side_left: false,
+            side_right: false,
+            top_center: false,
+            top_front_left: false,
+            top_front_center: false,
+            top_front_right: false,
+            top_back_left: false,
+            top_back_center: false,
+            top_back_right: false },
         sample_rate,
     };
 
@@ -106,9 +164,9 @@ struct SineIterator {
 
 // Used when writing via iterator
 impl Iterator for SineIterator {
-    type Item = Result<Vec<f32>>;
+    type Item = Result<SamplesByChannel<f32>>;
 
-    fn next(&mut self) -> Option<Result<Vec<f32>>> {
+    fn next(&mut self) -> Option<Result<SamplesByChannel<f32>>> {
         let result = (self.current_sample / self.period * TAU).sin();
         self.current_sample += 1f32;
 
@@ -116,6 +174,24 @@ impl Iterator for SineIterator {
             self.current_sample = 0f32;
         }
 
-        return Some(Ok(vec![result]));
+        return Some(Ok(SamplesByChannel {
+            front_left: Some(result),
+            front_right: None,
+            front_center: None,
+            low_frequency: None,
+            back_left: None,
+            back_right: None,
+            front_left_of_center: None,
+            front_right_of_center: None,
+            back_center: None,
+            side_left: None,
+            side_right: None,
+            top_center: None,
+            top_front_left: None,
+            top_front_center: None,
+            top_front_right: None,
+            top_back_left: None,
+            top_back_center: None,
+            top_back_right: None }));
     }
 }
